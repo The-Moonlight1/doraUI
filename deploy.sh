f2cec5be7d7ee13ee099c1565b0f3c442a108f3c
yangaoqi <2669526712@qq.com>
2023-07-27 11:22:26 +0800

    feat:添加Github Pages

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+pnpm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+
+git add .
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/The-Moonlight1/doraUI.git master:gh-pages
+
+cd -
