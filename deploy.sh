c9de51530df3a59242f86ae36465321c7dd54405
yangaoqi <2669526712@qq.com>
2023-07-27 13:32:29 +0800

    feat:只允许使用pnpm安装依赖

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,26 @@
-# 确保脚本抛出遇到的错误
+#!/usr/bin/env sh
+
+# 忽略错误
 set -e
 
-# 生成静态文件
-pnpm run docs:build
+# 构建
+npm run docs:build
 
-# 进入生成的文件夹
+# 进入待发布的目录
 cd docs/.vitepress/dist
 
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
 
-git add .
+git init
+git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/The-Moonlight1/doraUI.git master:gh-pages
+# 如果部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果是部署到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:The-Moonlight1/doraUI.git master:gh-pages
 
 cd -
+
