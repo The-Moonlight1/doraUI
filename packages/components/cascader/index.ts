836bd0bbb7d7888427d362e9e1804e7b045aee8c
yangaoqi <2669526712@qq.com>
2023-08-28 14:51:45 +0800

    feat: 删除 级联选择器与layout 组件

--- packages/components/cascader/index.ts
+++
@@ -1,4 +0,0 @@
-import cascader from './src/cascader.vue';
-import { withInstall } from '@dora-ui/utils';
-export const DoraCascader = withInstall(cascader);
-export default DoraCascader;
--- packages/components/components.ts
+++ packages/components/components.ts
@@ -1,11 +1,10 @@
 export { DoraButton } from './button';
 export { DoraIcon } from './icon';
 export { DoraTable } from './table';
-// export { DoraDivider } from './divider';
 export { DoraDivider } from './divider';
 export { DoraSpace } from './space';
 export { DoraLink } from './link';
-export { DoraCascader } from './cascader';
+// export { DoraCascader } from './cascader';
 export { DoraInput } from './input';
 export { DoraDataPicker } from './datapicker';
 export { DoraTooltip } from './tooltip';
